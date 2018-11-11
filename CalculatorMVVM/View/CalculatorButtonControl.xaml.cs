0c02d8c780ab62ac0f81bff57d37448c19565c04
Daniel Feijo <feijodan@gmail.com>
2018-11-11 01:30:50 -0500

    First commit

---
+++ CalculatorMVVM/View/CalculatorButtonControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CalculatorMVVM.ViewModel;
+using System.Windows.Controls;
+
+namespace CalculatorMVVM.View
+{
+    /// <summary>
+    /// Interaction logic for CalculatorButtonControl.xaml
+    /// </summary>
+    public partial class CalculatorButtonControl : UserControl
+    {
+        public string CalculatorButtonContent
+        {
+            get => _viewModel.CalculatorButtonContent;
+            set
+            {
+                _viewModel.CalculatorButtonContent = value;
+            }
+        }
+
+        private CalculatorButtonControlViewModel _viewModel;
+
+        public CalculatorButtonControl()
+        {
+            InitializeComponent();
+            _viewModel = new CalculatorButtonControlViewModel();
+            DataContext = _viewModel;
+        }
+    }
+}
---
+++ CalculatorMVVM/ViewModel/CalculatorButtonControlViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using GalaSoft.MvvmLight;
+
+namespace CalculatorMVVM.ViewModel
+{
+    public class CalculatorButtonControlViewModel : ViewModelBase
+    {
+        public string CalculatorButtonContent
+        {
+            get => _calculatorButtonContent;
+            set
+            {
+                _calculatorButtonContent = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _calculatorButtonContent;
+    }
+}
